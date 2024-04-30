e057d00ab802c537e61b6c2b36b22b16a7f63e90
CrisCL20 <cristian.soriano@usm.cl>
2024-04-30 18:13:46 -0400

    added navbar component to both admin and login view

---
+++ client/src/app/admin/page.jsx
@@ -0,0 +1,11 @@
+import { NavBar } from "../../components/NavBar";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+const AdminView = () => {
+    return (
+        <div class="container-fluid">
+            <NavBar />
+            
+        </div>
+    )
+}

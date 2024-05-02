7dab76637b210786c925179d7e26f0e0fd538381
CrisCL20 <cristian.soriano@usm.cl>
2024-05-01 22:51:00 -0400

    added tailwind

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -1,24 +1,6 @@
 /** @type {import('next').NextConfig} */
-import path from 'path';
 const nextConfig = {
-    webpack(config, options) {
-        config.modules.rules.push({
-            test: /\.scss$/,
-            use: [
-                options.defaultLoaders.babel,
-                {
-                    loader: 'sass-loader',
-                    options: {
-                        sassOptions: {
-                            includePaths: [path.join(__dirname, 'styles')],
-                        },
-                    },
-                },
-            ],
-        });
 
-        return config;
-    },
 };
 
 export default nextConfig;

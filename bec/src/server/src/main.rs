63e77f0a627a71543bd00c9bd528a583791322df
CrisCL20 <cristian.soriano@usm.cl>
2024-06-04 19:20:50 -0400

    pls

--- bec/src/server/src/main.rs
+++ bec/src/server/src/main.rs
@@ -38,37 +38,3 @@ async fn main() -> std::io::Result<()> {
     .run()
     .await
 }
-mod api;
-mod model;
-mod repository;
-
-use actix_web::{ web::Data, App, HttpServer };
-use api::{
-    user_api::{create_user, get_user, put_user, delete_user,get_users},
-    prestamos_api::{create_prestamo, get_prestamo, put_prestamo, delete_prestamo,get_prestamos}
-};
-use repository::mongodb_repo::MongoRepo;
-
-#[actix_web::main]
-async fn main() -> std::io::Result<()> {
-
-    let db = MongoRepo::init().await;
-    let db_data = Data::new(db);
-    HttpServer::new(move || {
-        App::new()
-        .app_data(db_data.clone())
-        .service(create_user)
-        .service(get_user)
-        .service(put_user)
-        .service(delete_user)
-        .service(get_users)
-        .service(create_prestamo)
-        .service(get_prestamo)
-        .service(put_prestamo)
-        .service(delete_prestamo)
-        .service(get_prestamos)
-    })
-    .bind(("127.0.0.1", 8080))?
-    .run()
-    .await
-}
--- bec/src/server/src/repository/mongodb_repo.rs
+++ bec/src/server/src/repository/mongodb_repo.rs
@@ -172,40 +172,6 @@ impl MongoRepo {
         Ok(document_detail.unwrap())
     }
 }
-impl MongoRepo {
-    pub async fn create_pres(&self, new_pres: Prestamo) -> Result<InsertOneResult, Error> {
-        let new_doc = Prestamo {
-            id : None,
-            id_ejemplar : new_pres.id_ejemplar,
-            tipo_prestamo : new_pres.tipo_prestamo,
-            fecha_prestamo : new_pres.fecha_prestamo,
-            hora_prestamo : new_pres.hora_prestamo,
-            hora_devolucion : new_pres.hora_devolucion,
-            fecha_devolucion : new_pres.fecha_devolucion,
-            fecha_devolucion_real : new_pres.fecha_devolucion_real,
-            hora_devolucion_real : new_pres.hora_devolucion_real,
-        };
-
-        let pres = self
-        .col_pres
-        .insert_one(new_doc, None)
-        .await
-        .ok()
-        .expect("Error creando prestamo");
-
-        Ok(pres)
-    }
-    
-    pub async fn get_prestamo(&self, pres_id: &String) -> Result<Prestamo, Error> {
-        let o_id = ObjectId::parse_str(pres_id).unwrap();
-        let filter = doc! {"_id" : o_id};
-        let prestamo_detail = self
-        .col_pres
-        .find_one(filter, None)
-        .await
-        .expect("Error getting pres detail.");
-    }
-}
 
 impl MongoRepo {
     pub async fn create_pres(&self, new_pres: Prestamo) -> Result<InsertOneResult, Error> {

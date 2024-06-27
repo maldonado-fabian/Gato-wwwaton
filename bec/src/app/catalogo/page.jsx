27aa404513434ceae0514cd27e96f42a06dfe77d
Fabian Maldonado <40547901+maldonado-fabian@users.noreply.github.com>
2024-06-27 08:13:23 -0400

    a

--- bec/src/app/catalogo/page.jsx
+++ bec/src/app/catalogo/page.jsx
@@ -1,19 +1,24 @@
 "use client"
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
-import Libro from "../../components/libro"
-import FlowNav from '@/components/navbar';
+import Libro from "../../components/libro";
+import Flownav from "../../components/navbar";
+import { SideBar } from "../../components/sidebar";
+import { Pagination } from 'flowbite-react';
 
 const App = () => {
   const [libros, setLibros] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const librosPerPage = 8; // Puedes ajustar la cantidad de libros por página
 
   useEffect(() => {
     const fetchLibros = async () => {
       try {
         const response = await axios.get('http://localhost:8080/documents'); // Reemplaza con tu endpoint real
         const librosData = response.data;
-
+        const API_KEY = 'AIzaSyD_mjS24t8B352rBPSU3la7DajnBSDw1iY'
         // Formatear título y autor, y obtener URL de la imagen del libro
+        
         const fetchBookCovers = async () => {
           const formattedLibros = await Promise.all(librosData.map(async (libro) => {
             const tituloFormateado = libro.titulo.replace(/\s+/g, '+');
@@ -54,20 +59,38 @@ const App = () => {
     fetchLibros();
   }, []);
 
+  // Calcular los índices de los libros de la página actual
+  const indexOfLastLibro = currentPage * librosPerPage;
+  const indexOfFirstLibro = indexOfLastLibro - librosPerPage;
+  const currentLibros = libros.slice(indexOfFirstLibro, indexOfLastLibro);
+
+  // Función para cambiar de página
+  const onPageChange = (pageNumber) => setCurrentPage(pageNumber);
+
   return (
     <div>
-      <FlowNav></FlowNav>
-    <div className="container mx-auto p-4 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
-      {libros.map((libro) => (
-        <Libro
-          key={libro.id} // Asegúrate de tener un identificador único para cada libro
-          url={libro.imageUrl}
-          titulo={libro.titulo}
-          autor={libro.autor}
-          año={libro.año}
-        />
-      ))}
-    </div>
+      <Flownav />
+      <div className='grid grid-cols-6 pt-10'>
+        <div className='col-span-2'><SideBar /></div>
+        <div className='col-span-4'>
+          <div className='grid grid-cols-4 gap-x-10 gap-y-10'>
+            {currentLibros.map((libro) => (
+              <Libro
+                key={libro.id} // Asegúrate de tener un identificador único para cada libro
+                url={libro.imageUrl}
+                titulo={libro.titulo}
+                autor={libro.autor}
+                año={libro.año}
+              />
+            ))}
+          </div>
+          <Pagination
+            currentPage={currentPage}
+            totalPages={Math.ceil(libros.length / librosPerPage)}
+            onPageChange={onPageChange}
+          />
+        </div>
+      </div>
     </div>
   );
 };
